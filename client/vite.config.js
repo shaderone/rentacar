0003fe79761cf137f9e6862a7de3dd7fcdb30984
sharon shaji <sharonshaji127001@gmail.com>
2025-12-30 18:25:44 +0530

    Bug fix : add proxy field. caused 404

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,10 +6,12 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
-    '/api': {
-      target: 'http://localhost:5000',
-      changeOrigin: true, // it means to change the origin of the host or domain of the request to the target URL. here's an example: if your frontend is running on http://localhost:3000 and you make a request to /api/data, with changeOrigin set to true, the request will appear to come from http://localhost:5000 instead of http://localhost:3000.
-      secure: false // in simple terms, setting secure to false allows the proxy to accept and work with self-signed or invalid SSL certificates without throwing errors.
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5000',
+        changeOrigin: true, // it means to change the origin of the host or domain of the request to the target URL. here's an example: if your frontend is running on http://localhost:3000 and you make a request to /api/data, with changeOrigin set to true, the request will appear to come from http://localhost:5000 instead of http://localhost:3000.
+        secure: false // in simple terms, setting secure to false allows the proxy to accept and work with self-signed or invalid SSL certificates without throwing errors.
+      }
     }
   }
 })
